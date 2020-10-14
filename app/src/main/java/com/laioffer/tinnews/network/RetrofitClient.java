41190e65138e18ccd0b714192b1e5fef53f6c5e5
Tianfeng Du <admiralight@gmail.com>
2020-10-14 13:04:59 -0400

    viewmodel and factory

--- app/src/main/java/com/laioffer/tinnews/network/RetrofitClient.java
+++ app/src/main/java/com/laioffer/tinnews/network/RetrofitClient.java
@@ -5,6 +5,7 @@ import android.content.Context;
 
 
 import com.ashokvarma.gander.GanderInterceptor;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
 
 import java.io.IOException;
 
@@ -25,6 +26,7 @@ public class RetrofitClient {
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .addInterceptor(new HeaderInterceptor())
                 .addInterceptor(new GanderInterceptor(context).showNotification(true))
+                .addNetworkInterceptor(new StethoInterceptor())
                 .build();
         return new Retrofit.Builder()
                 .baseUrl(BASE_URL)
