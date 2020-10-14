a65c23f1d6926264333c2e78d3cd1258ba45651b
Tianfeng Du <admiralight@gmail.com>
2020-10-14 15:12:16 -0400

    room database

--- app/src/main/java/com/laioffer/tinnews/model/Article.java
+++ app/src/main/java/com/laioffer/tinnews/model/Article.java
@@ -1,12 +1,56 @@
 package com.laioffer.tinnews.model;
 
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Objects;
+
+@Entity
 public class Article {
     public String author;
     public String content;
     public String description;
     public String publishedAt;
     public String title;
+    public boolean favorite;
+
+    @NonNull
+    @PrimaryKey
     public String url;
     public String urlToImage;
-    public boolean favorite;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return favorite == article.favorite &&
+                Objects.equals(author, article.author) &&
+                Objects.equals(content, article.content) &&
+                Objects.equals(description, article.description) &&
+                Objects.equals(publishedAt, article.publishedAt) &&
+                Objects.equals(title, article.title) &&
+                url.equals(article.url) &&
+                Objects.equals(urlToImage, article.urlToImage);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, content, description, publishedAt, title, favorite, url, urlToImage);
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "author='" + author + '\'' +
+                ", content='" + content + '\'' +
+                ", description='" + description + '\'' +
+                ", publishedAt='" + publishedAt + '\'' +
+                ", title='" + title + '\'' +
+                ", favorite=" + favorite +
+                ", url='" + url + '\'' +
+                ", urlToImage='" + urlToImage + '\'' +
+                '}';
+    }
 }
